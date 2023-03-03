0c7012b37c40046d9b654ae212cd951d7c898bf7
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-03-03 14:35:01 -0300

    4/10

---
+++ src/easy/reverse_string/__init__.py
@@ -0,0 +1,5 @@
+# https://leetcode.com/problems/reverse-string/
+
+
+def reverse_string(s: list[str]) -> list[str]:
+    return s[::-1]
---
+++ src/easy/reverse_string/test_reverse_string.py
@@ -0,0 +1,15 @@
+from . import reverse_string
+
+
+def test_1_reverse_string():
+    input: list[str] = ["h", "e", "l", "l", "o"]
+    output: list[str] = reverse_string(input)
+
+    assert output == ["o", "l", "l", "e", "h"]
+
+
+def test_2_reverse_string():
+    input: list[str] = ["H", "a", "n", "n", "a", "h"]
+    output: list[str] = reverse_string(input)
+
+    assert output == ["h", "a", "n", "n", "a", "H"]
